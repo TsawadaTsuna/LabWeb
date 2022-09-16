1a4bcdbba2fc041b01ad4335e8dfa029e9070aac
Jose Ignacio Ferrer Vera <joseferrertec@gmail.com>
2022-09-16 18:39:30 -0500

    Funciona Post request para prueba de datos

--- client/sw.js
+++ client/sw.js
@@ -1,29 +1,12 @@
 self.addEventListener("fetch", function(event) {
-  
-  /*/
-  $.put('/testdata', {"fecha" : "16/09/2022",
-  "hora" : "14:00",
-  "lat" : "123456",
-  "long" : "123456"})
-  */
-  
-  fetch("/testdata",{
-    method: 'POST',
-    headers:{
-      'Content-Type':'application/json'
-      },
-    params: JSON.stringify({"fecha" : "17/09/2022",
-    "hora" : "15:00",
-    "lat" : "123456",
-    "long" : "123456"})
-  })
 
   event.respondWith(
-    fetch(event.request).catch(function() {
+    fetch(event.request)
+    .catch(function() {
       return new Response(
-        //archivo
         
         );
     })
   );
+
 });
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const client = new MongoClient(uri);
 
 
 app.use(express.static('client'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 async function newPoint(client, data){
     try{
@@ -41,83 +43,55 @@ async function conectToPoints(client){
     const points = client.db("Rutas").collection("Puntos");
     const cursor = points.find({}, {projection:{ _id: 0 }});
     await cursor.forEach(console.dir);
-    } finally {
-      await client.close();
-    }
-  }
+} finally {
+    await client.close();
+}
+}
 
 async function getPoints() {
-	// we'll add code here soon
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-
+    // we'll add code here soon
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        
       // Make the appropriate DB calls
       //await  listDatabases(client);
       await conectToPoints(client);
-
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
+      
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
 
+//Get home page (index.html)
+app.get('/', (req, res) =>{
+    res.sendFile('./client/index.html', {title:'pwa'});
+});
 
+/*
 app.get("/testdataget", (req, res) => {
     getPoints().catch(console.error);
     res.end("Pets");
 })
+*/
 
-app.post("/testdata", (req, res) => {
+//Standard api call
+app.post('/api', (req, res) => {});
+
+app.post('/api/testdata', (req, res) => {
     let params = req.body
     console.log(params)
-    /*
     data = {
-        "fecha" : "16/09/2022",
-        "hora" : "14:00",
-        "lat" : "123456",
-        "alt" : "123456"
-    }*/
-    /*
-    data = {
-        "fecha" : params.fecha,
-        "hora" : params.hora,
-        "lat" : params.lat,
-        "long" : params.long
-    }*/
-    //upload(data).catch(console.error);
-        res.end("Point inserted");
+        fecha : params.fecha,
+        hora : params.hora,
+        lat : params.lat,
+        long : params.long
+    }
+    upload(data).catch(console.error);
+    res.end("Point inserted");
   })
-
-app.get("/testdata", (req, res) => {
-    getPoints().catch(console.error);
-    res.end("Pets");
-})
-
-
-app.get('/api', (req, res) => {
-//res.json({ message: 'Hello from server side!' });
-//res.sendFile('./pwa_cache_fj2022.html', {title:'Titulo'});
-/*fs.readFile('client/manifest.json', function(err, html){
-    if (err) {
-        throw err; 
-    }       
-    res.write(html);
-})*/
-/*fs.readFile('client/sw.js', function(err, html){
-    if (err) {
-        throw err; 
-    }       
-    res.send(html);
-})*/
-fs.readFile('client/pwa_cache_fj2022.html', function(err, html){
-    if (err) {
-        throw err; 
-    }       
-    res.write(html);
-})
-});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
