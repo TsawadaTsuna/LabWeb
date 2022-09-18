e0533b3984ada1da5d5a75a84f9df68d31154c17
Jose Ignacio Ferrer Vera <joseferrertec@gmail.com>
2022-09-18 13:41:23 -0500

    Inicio de funcion de service workers, implementando cambios en el dom dependiendo de estado de la conexion a internet

--- client/sw.js
+++ client/sw.js
@@ -1,12 +1,14 @@
+import fs from 'fs-extra';
+
 self.addEventListener("fetch", function(event) {
-
-  event.respondWith(
-    fetch(event.request)
-    .catch(function() {
-      return new Response(
-        
-        );
-    })
-  );
-
+  event.respondWith(async function() {
+    const cache = await caches.open('mysite-dynamic');
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) return cachedResponse;
+    const networkResponse = await fetch(event.request);
+    event.waitUntil(
+      cache.put(event.request, networkResponse.clone())
+    );
+    return networkResponse;
+  }());
 });
